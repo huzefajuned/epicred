8edc88b744e31341c06aa92c43852d60f6769f89
huzefa <huzefaghurna@gmail.com>
2025-02-09 20:09:44 +0530

    fix : login btn size

--- app/_components/ui/Login-Button.tsx
+++ app/_components/ui/Login-Button.tsx
@@ -6,7 +6,7 @@ const LoginButton = () => {
   return (
     <button
       onClick={signInWithGoogle}
-      className="bg-white text-black  hover:bg-black hover:text-white rounded-full text-sm sm:text-xl  border-2 px-5 py-1 sm:px-4 sm:py-1 text-md w-20 h-10 sm:w-28 sm:h-14"
+      className="bg-white text-black  hover:bg-black hover:text-white rounded-full text-sm sm:text-xl  border-2 px-2 py-1 sm:px-0 sm:py-0 text-md w-20 h-10  sm:w-28 sm:h-10"
     >
       Login
     </button>
